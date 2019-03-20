665999abdd0904e266ee66fc57244cab79155c02
enter3017sky <enter3017sky@gmail.com>
2019-03-20 21:27:29 +0800

    Updates

---
+++ precache-manifest.57becdb9e4bc8cf071ebf0c08cd0f96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f864bb5fbea68e95ca9",
+    "url": "/react-todo/./static/css/main.db38420a.chunk.css"
+  },
+  {
+    "revision": "9f864bb5fbea68e95ca9",
+    "url": "/react-todo/./static/js/main.6aa9a8db.chunk.js"
+  },
+  {
+    "revision": "a19a484131ed1b1331af",
+    "url": "/react-todo/./static/js/runtime~main.a756ba8e.js"
+  },
+  {
+    "revision": "c09746ee5955a773739d",
+    "url": "/react-todo/./static/css/2.cdf76e4a.chunk.css"
+  },
+  {
+    "revision": "c09746ee5955a773739d",
+    "url": "/react-todo/./static/js/2.b6ac0255.chunk.js"
+  },
+  {
+    "revision": "47a19ddef9815713ea338873d6ee5457",
+    "url": "/react-todo/./index.html"
+  }
+];
